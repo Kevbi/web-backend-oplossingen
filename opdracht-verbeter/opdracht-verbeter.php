7e7556a8a33e49c1af1e76879fb0d4585718365b
kevin <kevin.buys@student.kdg.be>
2016-09-30 15:41:20 +0200

    opdracht verbeter

---
+++ opdracht-verbeter/opdracht-verbeter.php
@@ -0,0 +1,19 @@
+<?php
+
+	$tekst 			= 	'Test 123...';
+	$_2_tekst 		= 	'Test 456';
+	$_tekst 		= 	'test';
+	$zi_n 			= 	'Jean D\'Haeseleir gaat naar Praag!!';
+	$beschrijving 	= 	"Vogelbekdier'"
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+	<h1>Foutjes verbeteren</h1>
+
+</body>
+</html>
