7bc4a3d8ea84b0452ce3bbe4bdb422fee5840197
kevin <kevin.buys@student.kdg.be>
2016-10-13 14:33:15 +0200

    opdracht functions + foreach

---
+++ opdracht-functions/opdracht-functions.php
@@ -0,0 +1,36 @@
+<?php
+
+	function berekenSom($getal1,$getal2) {
+		return $getal1 + $getal2;
+	}
+
+	function vermenigvuldig($getal1,$getal2) {
+		return $getal1 * $getal2;
+	}
+
+	function isEven($getal) {
+		($getal%2 == 0) ? "true" : "false";
+	}
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Opdracht functies</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+    </head>
+    <body class="web-backend-opdracht">
+        
+        <section class="body">
+        
+            <h1>Opdracht functies: deel 1</h1>
+            <p><?= isEven(8); ?></p>
+
+        </section>
+
+    </body>
+</html>
