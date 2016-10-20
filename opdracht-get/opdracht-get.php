82b0537e9ae4788315f379bc7466c0dd56299335
kevin <kevin.buys@student.kdg.be>
2016-10-20 16:14:10 +0200

    update artikels get

--- opdracht-get/opdracht-get.php
+++ opdracht-get/opdracht-get.php
@@ -12,10 +12,25 @@
 Daarbij wordt fors ingezet op intelligente veiligheidssystemen. De uitbouw van een ANPR-cameraschild (nummerplaatherkenning) is al langer bezig, en dergelijke camera's zullen ook gebruikt worden om begin volgend jaar een slagboomsysteem te installeren op de toegangsweg tot de luchthaven. Daarnaast zal er op de luchthaven gewerkt worden met gezichtsherkenningscamera's.
 
 Wat de menselijke inzet betreft, krijgen de agenten op de luchthaven een opleiding om verdachte gedragingen te kunnen herkennen, naar het voorbeeld van de 'behaviour detection officers' op de Israëlische luchthaven Ben Gurion. 'Het gaat echt om het herkennen van bepaalde gedragingen, dus niet op basis van leeftijd, geslacht of etnie', aldus Jambon. Daarnaast komt er een snelleresponsteam en de federale politie zal aanwezig zijn in het operationeel centrum van de luchthaven.";
-    $artikels[0]["afbeelding"] = "booo";
-    $artikels[0]["afbeeldingBeschrijving"] = "booo";
+    $artikels[0]["afbeelding"] = "/img/foto.png";
+    $artikels[0]["afbeeldingBeschrijving"] = "foto ingang luchthaven";
 
-    var_dump($_GET);
+    $artikels[1]["titel"] = "Rusland maakt trajecten Belgische F-16's in Syrië openbaar";
+    $artikels[1]["datum"] = "20/10/16";
+    $artikels[1]["inhoud"] = "OORLOG IN SYRIË Volgens het Russische ministerie van Defensie bewijzen radarbeelden dat Belgische F-16's wel degelijk een 'bombardement' hebben uitgevoerd op een dorp nabij het noord-Syrische Aleppo. Rusland zal de vliegroutes van de gevechtstoestellen op het ogenblik van het dodelijk bombardement openbaar maken. Volgens het ministerie zijn de twee Belgische F-16's van op een luchthaven in Jordanië opgestegen en zijn twee keer in de lucht door een Amerikaans tankvliegtuig bijgetankt.";
+    $artikels[1]["afbeelding"] ="img/foto2.jpg";
+    $artikels[1]["afbeeldingBeschrijving"] = "foto F-16 met piloot" ;
+
+    $artikels[2]["titel"] = "Nieuwe studie toont aan dat er 40 jaar geleden waarschijnlijk tóch leven werd gevonden op Mars";
+    $artikels[2]["datum"] = "20/10/2016";
+    $artikels[2]["inhoud"] = "Amerikaanse wetenschappers hebben nieuw licht geworpen op een Marsmissie van 40 jaar geleden en dat zou verstrekkende gevolgen kunnen hebben. In een bodemstaal werden toen sporen gevonden die op leven konden duiden, maar het resultaat werd alom in twijfel getrokken omdat controletests geen duidelijk uitsluitsel gaven. Nieuw onderzoek toont nu aan dat de oorspronkelijke onderzoekers het tóch bij het juiste eind gehad zouden kunnen hebben.
+
+De nieuwe studie werd uitgevoerd door Gilbert Levin van de universiteit van Arizona en Patricia Ann Straat van het Nationaal Gezondheidsinstituut in Bethesda, Maryland. Allebei waren ze ook betrokken bij de oorspronkelijke missie van de Vikingsatellieten en hun landers in 1976. Het was toen de meest ambitieuze poging van de wetenschap om de rode planeet te verkennen. ";
+    $artikels[2]["afbeelding"] = "img/foto3.jpg";
+    $artikels[2]["afbeeldingBeschrijving"] = "foto mars";
+
+
+    #var_dump($_GET);
 
     $apart = false;
 
@@ -39,6 +54,10 @@ Wat de menselijke inzet betreft, krijgen de agenten op de luchthaven een opleidi
                 width: 20%;
                 background-color: grey; 
                 float:left;           
+            }
+            .meerdere img {
+                width: 250px;
+                margin: 5%;
             }
             .apart {
                 margin:10%;
