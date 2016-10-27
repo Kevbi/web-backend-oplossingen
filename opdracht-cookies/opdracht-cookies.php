3b2bc3bef81a0658803ba2993292ca7239f694bd
kevin <kevin.buys@student.kdg.be>
2016-10-27 15:22:37 +0200

    opdracht cookies

---
+++ opdracht-cookies/opdracht-cookies.php
@@ -0,0 +1,68 @@
+<?php
+
+    $txt = file_get_contents("test.txt");
+    $txtArray = explode(",", $txt);
+    $fout = false;
+    $isAuthenticated = false;
+
+
+
+    if (isset($_POST["id"]) && isset($_POST["psw"]) ) {
+        if ($_POST["id"] == $txtArray[0] && $_POST["psw"] == $txtArray[1]) {
+            
+            setcookie( 'authenticated', TRUE, time() + 360 ); 
+         }
+        else{
+           $fout = true;
+        }
+    }
+
+    //als cookie bestaaat authenticated to true
+    if (isset($_COOKIE["authenticated"])) {
+        $isAuthenticated    =   true;
+    }
+
+    if (isset($_GET["logout"])) {
+    
+            setcookie('authenticated',"", time() -3600);
+            
+            header('location: opdracht-cookies.php');
+    }
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="wi$cookieArray = dth=device-width, initial-scale=1">
+        <title>Opdracht cookies</title>
+        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+    </head>
+    <body class="web-backend-opdracht">
+    
+    <section class="body">
+
+        <?php if ($isAuthenticated): ?>
+        <p>U bent ingelogd.</p>
+        <a href="opdracht-cookies.php?logout">uitloggen</a>
+        <?php endif ?>
+        <?php if (!$isAuthenticated): ?>
+        <h1>Inloggen</h1>
+        <?php endif ?>
+        <?php if ($fout): ?>
+        <p><code>Gebruikersnaam en/of paswoord niet correct. Probeer opnieuw.</code></p>
+        <?php endif ?>
+
+        <?php if (!$isAuthenticated) :?>
+        <form method="post">
+            <label>gebruikersnaam</label>
+            <input type="text" name="id">
+            <label>paswoord</label>
+            <input type="password" name="psw"> </br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+       <?php endif ?>
+     </section>
+    </body>
+</html>
